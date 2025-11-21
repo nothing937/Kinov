f920949310dfcdd108fb9c5badb2733cfed5de68
nothing937 <jayden2006lee@gmail.com>
2025-11-21 23:02:32 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import Contact from './contact';
+import Videos from './videos';
 
 export default function App() {
   return (
